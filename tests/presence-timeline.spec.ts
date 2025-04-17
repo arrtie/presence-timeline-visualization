f913f06c959575f658bd8ce7d1e8b46128228836
Artie <artierosser@gmail.com>
2025-04-17 22:05:18 +0000

    fixed tests

--- tests/presence-timeline.spec.ts
+++ tests/presence-timeline.spec.ts
@@ -1,4 +1,6 @@
-import { test, expect, type Page, type Locator } from '@playwright/test';
+/** @format */
+
+import { test, expect, type Page, type Locator } from "@playwright/test";
 
 // TODO: test for the failure of GET /profiles
 // TODO: test for profile avatars
@@ -8,106 +10,121 @@ function getPresenceBubble(identifier: string) {
 }
 
 async function getPresenceBubbleY(bubble: Locator) {
-  return (await bubble.boundingBox())?.y
+  return (await bubble.boundingBox())?.y;
 }
 
-test.describe('when the presence data is loading', () => {
-  test.beforeEach(async ({page}) => {
-    await page.route('*/**/api/presence', async route => new Promise(()=> {}));
-  })
+test.describe("when the presence data is loading", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.route(
+      "*/**/api/presence",
+      async (route) => new Promise(() => {})
+    );
+  });
 
-  test('should find loader', async ({ page }) => {
-    await page.goto('/');
+  test("should find loader", async ({ page }) => {
+    await page.goto("/");
     await expect(page.getByTestId("loader")).toBeVisible();
   });
 });
 
-test.describe('when there is a network error fetching the presence data', () => {
-  test.beforeEach(async ({page}) => {
-    await page.route('*/**/api/presence', async route => {
+test.describe("when there is a network error fetching the presence data", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.route("*/**/api/presence", async (route) => {
       await route.abort();
     });
-  })
+  });
 
-  test('should find the missing presence data warning', async ({ page }) => {
-    await page.goto('/');
-    await expect(page.getByTestId("missing-presence-data")).toHaveText("Presence data is missing :(");
-  })
+  test("should find the missing presence data warning", async ({ page }) => {
+    await page.goto("/");
+    await expect(page.getByTestId("missing-presence-data")).toHaveText(
+      "Presence data is missing :("
+    );
+  });
 });
 
-test.describe('when the presence data is empty', () => {
-  test.beforeEach(async ({page}) => {
-    await page.route('*/**/api/presence', async route => {
+test.describe("when the presence data is empty", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.route("*/**/api/presence", async (route) => {
       const json = null;
       await route.fulfill({ json });
     });
-  })
+  });
 
-  test('should find the missing presence data warning', async ({ page }) => {
-    await page.goto('/');
-    await expect(page.getByTestId("missing-presence-data")).toHaveText("Presence data is missing :(");
-  })
+  test("should find the missing presence data warning", async ({ page }) => {
+    await page.goto("/");
+    await expect(page.getByTestId("missing-presence-data")).toHaveText(
+      "Presence data is missing :("
+    );
+  });
 });
 
-test.describe('when there is presence data', () => {
-  test.describe('and there is only one profile', () => {
+test.describe("when there is presence data", () => {
+  test.describe("and there is only one profile", () => {
+    function generatePresenceIntervals(
+      duration: number,
+      gap: number,
+      count: number
+    ) {
+      const now = Date.now();
+
+      return Array(count)
+        .fill(null)
+        .map((_, index) => {
+          const current = now - (duration + gap) * index;
+          return [current - duration, current];
+        })
+        .reverse();
+    }
+
+    const presence_intervals = generatePresenceIntervals(
+      1000 * 60 * 60 * 2, // Two hours
+      1000 * 60 * 60 * 1, // One hour
+      4
+    );
+
     const singleUserWithMultiplePresence = {
       "85956": {
-          "presence_intervals": [
-              [
-                  1737053092803,
-                  1737067430558
-              ],
-              [
-                  1737067748658,
-                  1737069047321
-              ],
-              [
-                  1737558910173,
-                  1737571706016
-              ],
-              [
-                  1737573027870,
-                  1737581786422
-              ],
-              [
-                  1737673603498,
-                  1737698399999
-              ]
-          ],
-          "current_status": "present"
-      }
-    }
-    
-    test.beforeEach(async ({page}) => {
-      await page.route('*/**/api/presence', async route => {
-        const json = JSON.stringify(singleUserWithMultiplePresence);
+        presence_intervals: presence_intervals,
+        current_status: "present",
+      },
+    };
+
+    test.beforeEach(async ({ page }) => {
+      await page.route("*/**/api/presence", async (route) => {
+        const json = singleUserWithMultiplePresence;
         await route.fulfill({ json });
       });
-    })
-    
-    test('should find that profile\'s presence entries and exits in order down the Y axis', async ({ page }) => {
-      await page.goto('/');
-      for(const [profile, presenceData] of Object.entries(singleUserWithMultiplePresence)) {
+    });
+
+    test.only("should find that profile's presence entries and exits in order down the Y axis", async ({
+      page,
+    }) => {
+      await page.goto("/");
+
+      for (const presenceData of Object.values(
+        singleUserWithMultiplePresence
+      )) {
         let lastYIndex = 0;
-        presenceData.presence_intervals.forEach(async (presenceInterval) => {
+
+        for (const presenceInterval of presenceData.presence_intervals) {
           const [enter, exit] = presenceInterval;
           const enterBubble = getPresenceBubble(enter.toString())(page);
-          expect(enterBubble).toBeVisible();
+          await expect(enterBubble).toBeVisible();
           const exitBubble = getPresenceBubble(exit.toString())(page);
-          expect(exitBubble).toBeVisible();
+          await expect(exitBubble).toBeVisible();
 
-          const enterBubbleY = await  getPresenceBubbleY(enterBubble);
-          const exitBubbleY =  await getPresenceBubbleY(exitBubble);
-          
-          if(enterBubbleY == null || exitBubbleY == null) {
-            throw new Error("could not find a y index of a presence bubble")
+          const enterBubbleY = await getPresenceBubbleY(enterBubble);
+          const exitBubbleY = await getPresenceBubbleY(exitBubble);
+
+          if (enterBubbleY == null || exitBubbleY == null) {
+            throw new Error("could not find a y index of a presence bubble");
           }
+
           expect(exitBubbleY).toBeGreaterThan(enterBubbleY);
           expect(exitBubbleY).toBeGreaterThan(lastYIndex);
           lastYIndex = exitBubbleY;
-        })
+        }
       }
-    })
+    });
   });
 });
